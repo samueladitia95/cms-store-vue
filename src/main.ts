01b147d46f332d8f0bcf761414fbe9a087f2af66
Samuel Aditia <samueladitia95@gmail.com>
2021-12-12 15:40:38 +0700

    Init tailwind css

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./routes";
 import { key, store } from "./store";
+import "./index.css";
 
 createApp(App).use(router).use(store, key).mount("#app");
