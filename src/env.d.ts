bf682920541a42edbc2439d43494a868fdf3a3a9
Samuel Aditia <samueladitia95@gmail.com>
2021-12-12 15:57:31 +0700

    Add dotenv config

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,18 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_SERVER_BASE_URL: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
 declare module "*.vue" {
   import { DefineComponent } from "vue";
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
