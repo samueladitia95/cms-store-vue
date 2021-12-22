d72a602939f1542b51a64d2adec4835d578afaf7
Samuel Aditia <samueladitia95@gmail.com>
2021-12-23 04:44:15 +0700

    Add safelist for width dan height

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  safelist: [
+    {
+      pattern: /[h, p]-[6, 10]/,
+    },
+  ],
   theme: {
     extend: {
       fontFamily: {
